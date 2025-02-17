19a01a64b4f3918eec04427ed9886138816991ba
Micael Junco <micaeljuncoplay4@gmail.com>
2025-02-17 17:01:34 -0300

    .

--- PYTHON/exercicos/numInver.py
+++ PYTHON/exercicos/numInver.py
@@ -1,9 +1,12 @@
 def leNumInt():
-    num = int(input('Digite um número inteiro: '))
-    return num
+    while True:
+        try:
+            return int(input('Digite um número inteiro: '))
+        except ValueError:
+            print("Entrada inválida. Digite um número inteiro.")
 
 def inverterVal(num):
-    num = str(num)[::-1]
+    num = str(num)[::-1] #slicing para inverter a string, o passo '-1' lê de forma invertida
     return int(num)
 
 
--- PYTHON/exercicos/potencia.py
+++ PYTHON/exercicos/potencia.py
@@ -6,7 +6,7 @@ def potenciaNumInt(num):
     potencia = int(input(f'Digite quantas vezes deseja multiplicar {num} por ele mesmo: '))
     res = 0
 
-    for _ in range(potencia-1):
+    for _ in range(potencia-1): #'-1' está aqui por que se, por exemplo, o expoente fosse 2 e a base 3, ele passaria pelo loop duas vezes e daria 18, como '3*3*3'. Isso pq na primeira vez que passa no for, já existe 'num * num'
         res = res + (num*num)
 
     return res
